3c192cf81028d8bcbdd5cedb59287bebe4caace6
Charansai3003 <23je0493@iitism.ac.in>
2026-09-03 17:27:32 +0530

    Complete user authentication APIs

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
-const express = require("express");
-
 require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 const PORT = 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Vehicle Rental System Backend is running!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
