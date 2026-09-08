bafc169f6c702723e9610bf188dcf48c00860a8d
Charansai3003 <23je0493@iitism.ac.in>
2026-09-08 15:15:11 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -30,6 +30,6 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
